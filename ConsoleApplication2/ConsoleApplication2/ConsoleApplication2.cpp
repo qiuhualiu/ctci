8738d76d4f73636fea0c415763373f7b98fe3711
Qiuhua Liu <liuqiuhua@yahoo.com>
2015-11-17 15:38:23 +0800

    .

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Question1_1.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Question1_1 q;
+
+	q.run();
+
+	return 0;
+}
+
